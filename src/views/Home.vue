7c6c98a732892024954fdf21938c4756e6da03d2
Saheera Rambali <saheerarambali14@gmail.com>
2019-11-20 03:58:02 -0400

    home view done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,223 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col xl6 s12">
+          <h3>Webpartments</h3>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img1.jpeg">
+                    <span class="card-title">Blue Mountain State</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <br>
+                    <i class="material-icons">wifi</i>
+                    <i class="material-icons">ac_unit</i>
+                    <i class="material-icons">local_taxi</i>
+                    <p>Single, shared rooms</p>
+                    <p>5 mins from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img2.jpeg">
+                    <span class="card-title">West Gate</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img3.jpeg">
+                    <span class="card-title">Silver Tree</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img4.jpeg">
+                    <span class="card-title">Level Towers</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img5.jpeg">
+                    <span class="card-title">Cherry House</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img6.jpeg">
+                    <span class="card-title">Ken Hall</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m7">
+            <div class="row">
+                <div class="col s12 m7">
+                <div class="card">
+                  <div class="card-image">
+                    <img src="img7.jpeg">
+                    <span class="card-title">Ryan's</span>
+                  </div>
+                  <div class="card-content">
+                    <i class="material-icons">attach_money</i>
+                    <i class="material-icons">attach_money</i>
+                    <p>Ammenities</p>
+                    <p>Types of rooms</p>
+                    <p>distance from campus</p>
+                  </div>
+                  <div class="card-action">
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons yellow-text text-darken-2">star</i>
+                    <i class="material-icons black-text">star</i>
+                    <i class="material-icons black-text">star</i>
+                  </div>
+                </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+<style>
+  li {
+    padding:20px;
+    border-radius: 7px;
+  }
+</style>
